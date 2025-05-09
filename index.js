8a26abfcdcd118073411e4f5d1f5d20e4af55f32
PinguimSurfante <163582730+PinguimSurfante@users.noreply.github.com>
2025-05-09 15:07:00 -0300

    style: atualizando css

---
+++ index.js
@@ -0,0 +1,13 @@
+//  Para cada 90 caracteres ele adiciona um breakpoint
+// Preciso selecionar o tamanho de cada description, no caso tem 2
+
+// $('.description').each(function () {
+//     let text = $(this).text();
+//     let newText = '';
+
+//     for (let i = 0; i < text.length; i += 80) {
+//         newText += text.substr(i, 80) + '<br>';
+//     }
+
+//     $(this).html(newText); 
+// });
